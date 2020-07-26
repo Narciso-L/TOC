7cb96bff2982d50a4d463099b3ad753ebc59bcd1
Narciso <33524932+Narciso-L@users.noreply.github.com>
2020-07-26 16:33:57 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/*Copyright (C) <2016>  <Narciso López-López>
+/*Copyright (C) <2017>  <Narciso López-López>
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
